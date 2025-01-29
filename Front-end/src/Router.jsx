ad73a28c30e7308f69620215cbb27cbffca0c22d
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-01-29 18:52:40 +0100

    Bases settings (index, router, app, css)

---
+++ Front-end/src/Router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/Layout"
+import Profile from "./pages/profile/Profile"
+import Error from "./pages/error/Error"
+
+const AppRouter = () => {
+    return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="user/:id" element={<Profile />} />
+          <Route path="error" element={<Error />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
+    )
+}
+
+export default AppRouter
