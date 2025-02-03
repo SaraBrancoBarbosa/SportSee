b6966591a0af841ecf758bfdf97f22940b6241b8
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-02-03 16:14:01 +0100

    Homepage added to Router

--- Front-end/src/Router.jsx
+++ Front-end/src/Router.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/Layout"
 import Profile from "./pages/profile/Profile"
 import Error from "./pages/error/Error"
+import Homepage from "./pages/homepage/Homepage"
 
 const AppRouter = () => {
     return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
           <Route path="user/:id" element={<Profile />} />
           <Route path="error" element={<Error />} />
           <Route path="*" element={<Error />} />
