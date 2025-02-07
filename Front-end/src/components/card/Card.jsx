50d30a1acd1cda4182caf721d5c49b2403e39417
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-02-07 18:43:47 +0100

    Card component

---
+++ Front-end/src/components/card/Card.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types"
+import "./card.css"
+
+// Card component for the key data
+
+const iconMap = {
+    "Calories": "/assets/icons/calorie.svg",
+    "Protéines": "/assets/icons/protein.svg",
+    "Glucides": "/assets/icons/carbohydrate.svg",
+    "Lipides": "/assets/icons/lipid.svg"
+}
+
+function Card({ backgroundColor, label, value }) {
+
+    const icon = iconMap[label] || ""
+
+    return (
+		<div className="card">
+
+			<div className="icon-container" style={{ backgroundColor: backgroundColor}}>
+                <img src={icon} className="icon" alt={label}></img>
+            </div>
+
+            <div className="text-container">
+                <p className="text-value">{value}</p>
+                <p className="text-label">{label}</p>
+            </div>
+				
+		</div>
+	)
+}
+
+Card.propTypes = {
+	icon: PropTypes.string,
+	backgroundColor: PropTypes.string,
+	label: PropTypes.string,
+    value: PropTypes.string,
+}
+
+export default Card
