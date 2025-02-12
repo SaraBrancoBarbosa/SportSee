07df1facb9f1545eccb6b4022944cb1bb67cabd3
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-02-12 20:04:49 +0100

    Index CSS update

--- Front-end/src/index.jsx
+++ Front-end/src/index.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ApiProvider from './api/context/ApiProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApiProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApiProvider>
 )
