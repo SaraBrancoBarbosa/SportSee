7db497db2712db20ede8d6ce691cb21f0126ae2b
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-02-14 15:33:51 +0100

    All information moved to Settings + swith API button

--- Front-end/src/pages/homepage/Homepage.jsx
+++ Front-end/src/pages/homepage/Homepage.jsx
@@ -1,15 +1,10 @@
-import { Link } from "react-router"
 import "./homepage.css"
 
 function Homepage() {
   return (
     <div className="main-container">
       
-        <h1>Les deux utilisateurs :</h1>
-    
-        <Link to="/user/12" className="id-link">Karl Dovineau, id 12</Link>
-
-        <Link to="/user/18" className="id-link">Cecilia Ratorez, id 18</Link>
+        <h1>Bienvenue sur l&apos;application <span className="span">SportSee</span> !</h1>
        
     </div>
   )
--- Front-end/src/pages/settings/Settings.jsx
+++ Front-end/src/pages/settings/Settings.jsx
@@ -1,32 +1,32 @@
 import { useContext } from "react"
-import useApi from "../../api/Api"
+import { Link } from "react-router"
 import ApiContext from "../../api/context/ApiContext"
 import "./settings.css"
 
 function Settings() {
     
     const { toggleMode, mode:apiMode } = useContext(ApiContext)
-  
-    const { api } = useApi()
 
+    const buttonText = apiMode === "mocked" 
+        ? <>Actuellement sur les données <span className="button-span">mockées</span></>
+        : <>Actuellement sur les données du <span className="button-span">Back-End</span></>
+  
     return (
         <div className="main-container">
         
             <h1>Switch les données :</h1>
         
             <div>
-                <button onClick={() => {
-                toggleMode()
-                }}>{apiMode}</button>
-
-                <button onClick={() => {
-                api.getUsers()
-                }}>getUsers</button>
-
-                <button onClick={() => {
-                api.getUserSession()
-                }}>getUserSession</button>
+                <button className="button" onClick={() => {toggleMode()}}>
+                    {buttonText}
+                </button> 
             </div>
+
+            <h1>Les deux utilisateurs :</h1>
+    
+            <Link to="/user/12" className="id-link">Karl Dovineau, id 12</Link>
+
+            <Link to="/user/18" className="id-link">Cecilia Ratorez, id 18</Link>
         
         </div>
     )
