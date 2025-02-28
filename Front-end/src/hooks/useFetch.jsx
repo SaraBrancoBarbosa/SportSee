15bb1acc7be581db2da65b617d2a1fd3b619a714
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-02-28 18:07:39 +0100

    setLoading set to false + loadUsers now named as fetchData for clarity + better setTimeout logic

--- Front-end/src/hooks/useFetch.jsx
+++ Front-end/src/hooks/useFetch.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react"
 
-/***** Custom hook to manage the data loading system *****/
+/***** Custom hook to manage the data fetching and loading system *****/
 
-const useFetch = (url, key, onLoaded=() => {}) => {
-  
+const useFetch = (url, key, onLoaded=() => {}, fetchDelay = 0) => {
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(false)
     const [loaded, setLoaded] = useState(false)
@@ -13,39 +12,58 @@ const useFetch = (url, key, onLoaded=() => {}) => {
     useEffect(() => {
         setError(null)
         setLoaded(false)
-        setLoading(true)
+        setLoading(false)
     }, [url, key])
 
     useEffect(() => {
         // Launches only if loading is false, and if loaded is false
         if(loading || loaded) return
 
+        if(!url) return
+
         // Async func inside useEffect
-        const loadUsers = async() => {
-                setTimeout(() => {
-                    fetch(url)
-                    .then(response => {
-                        return response.json()
-                    })
-                    .then(data => {
-                        // Formatting the retrieved data, stored in data
-                        setData(onLoaded?onLoaded(data):data)
-                        setLoaded(true)
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                        setError("Oh non une erreur !")
-                    })
-                    .finally(() => {
-                        setLoading(false)
-                    })
-                // Loading simulation    
-                }, 250)
+        const fetchData = async() => {
+            setLoading(true)
+
+            const fetchCallback = () => {
+                fetch(url)
+                .then(response => {
+                    if (!response.ok) {throw new Error ("Erreur serveur")}
+                    // First formatting => .json
+                    return response.json()
+                })
+                .then(data => {
+                    /***** 
+                    The onLoaded function (callback) is called as soon as useFetch has finished loading
+                    When returned, the data is formatted as wanted: each fetch will have different formatting, since each object is different
+                    Then the function is returned with onLoaded which contains "data": its role is to standardise the data
+                    *****/
+                    setData(onLoaded?onLoaded(data):data)
+                    setLoaded(true)
+                    setLoading(false)
+                })
+                .catch((error) => {
+                    console.log(error)
+                    setError("Oh non une erreur !")
+                    setLoading(false)
+                })
+            }
+            /***** 
+            If fetchDelay is > 0, then the fetch is delayed by a setTimeout, and the delay is applied before the fetch occurs
+            It allows to simulate a loading time to test the user interface in dev mode
+            When fetchDelay is not > 0, setTimeout will not be used and the fetch request occurs immediately.
+            import.met.env.DEV allows to detect the environment: dev mode with mocked data (=> value: true) or prod mode with real data (=> value: false)
+            *****/
+            if(import.meta.env.DEV && !isNaN(fetchDelay) && fetchDelay > 0) {
+                setTimeout(fetchCallback, fetchDelay)
+            } else {
+                fetchCallback()
+            }
         } 
 
-        loadUsers()
+        fetchData()
 
-    }, [url, loaded, loading, onLoaded])
+    }, [url, loaded, loading, onLoaded, fetchDelay])
 
     return {
         data,
